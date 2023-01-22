ed4f3a5016deaf45d0a25c24545a6e5eee48f037
idilipsarker <dilipsarker719@gmail.com>
2023-01-22 15:02:12 +0600

    local storage and cart calcualation added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -15,10 +16,35 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
-  const handleAddToCart = (product) => {
-    console.log("clicked", product);
-    const newCart = [...cart, product];
+  //get saved cart from local storage
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    console.log(storedCart);
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart);
+  }, [products]);
+
+  const handleAddToCart = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find((product) => product.id === selectedProduct.id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
     setCart(newCart);
+    addToDb(selectedProduct.id);
   };
 
   return (
