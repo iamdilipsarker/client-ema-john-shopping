7fe5a1d2079a8d0670490d892d6ded5b376e9f3e
idilipsarker <dilipsarker719@gmail.com>
2023-03-16 11:45:59 +0600

    pagination implemented

--- src/loaders/productsAndCartLoader.js
+++ src/loaders/productsAndCartLoader.js
@@ -2,19 +2,19 @@ import { getStoredCart } from "../utilities/fakedb";
 
 export const productsAndCartLoader = async () => {
   //get products
-  const productsData = await fetch("products.json");
-  const products = await productsData.json();
+  const productsData = await fetch("http://localhost:5000/products");
+  const { products, count } = await productsData.json();
 
   //get cart
   const savedCart = getStoredCart();
   const storedCart = [];
   for (const id in savedCart) {
-    const addedProduct = products.find((product) => product.id === id);
+    const addedProduct = products.find((product) => product._id === id);
     if (addedProduct) {
       const quantity = savedCart[id];
       addedProduct.quantity = quantity;
       storedCart.push(addedProduct);
     }
   }
-  return { products, storedCart };
+  return { products, count, storedCart };
 };
